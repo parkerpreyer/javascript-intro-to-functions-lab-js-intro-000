7be9948398b7e1e8cae10ed393cbac738d1e60b2
Parker Preyer <parker.preyer@gmail.com>
2019-10-28 17:48:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+sayHiToGrandma(string) {
+  let lowercase = string.toLowerCase()
+  let uppercase = string.toUpperCase()
+  let love = "I love you, Grandma."
+
+  if (string === lowercase) {
+    return "I can'\t hear you!"
+  } else if (string === uppercase) {
+    return "YES INDEED!"
+  } else if (string === love) {
+    return love
+  }
+}
